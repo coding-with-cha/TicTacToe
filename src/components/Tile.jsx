7aff5c578a0e748da7b2653d8f4b0c002758187c
CHAIMA MEJBRI <chaimamejbri@gmail.com>
2024-06-18 20:14:03 +0200

    first commit

---
+++ src/components/Tile.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Tile = ({className, value, onClick}) => {
+  return (
+    <div onClick={onClick} className={`tile ${className}`}>{value}</div>
+  )
+}
+
+export default Tile
